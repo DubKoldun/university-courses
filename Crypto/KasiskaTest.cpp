97d51b23e274e821039c97228d3e7d0aa4aa7bdc
u-menya-lapki <komaroff404@gmail.com>
2019-09-26 02:16:13 +0300

    encryptor added (doesn't work with strange chars (???))

--- Crypto/KasiskaTest.cpp
+++ Crypto/KasiskaTest.cpp
@@ -8,6 +8,34 @@ using std::vector;
 using std::cout;
 using std::cin;
 
+const size_t MAX_PERIOD = 17;
+const size_t ALPHABET_LENGTH = 26;
+vector<vector<char>> vigenereSquare(ALPHABET_LENGTH, vector<char>(ALPHABET_LENGTH));
+
+void generateVigenereSquare()
+{
+    for (size_t i = 0; i < ALPHABET_LENGTH; ++i)
+    {
+        for (size_t j = 0; j < ALPHABET_LENGTH; ++j)
+        {
+            vigenereSquare[i][j] = ('A' + j + i > 'Z' ? ('A' + j + i - ALPHABET_LENGTH) : ('A' + j + i));
+        }
+    }
+}
+
+void printVigenereSquare()
+{
+    for (size_t i = 0; i < vigenereSquare.size(); ++i)
+    {
+        for (char c : vigenereSquare[i])
+        {
+            cout << c << ' ';
+        }
+        cout << std::endl;
+    }
+}
+
+
 double indexAlpha(string text, size_t period)
 {
     vector<double> currentAlpha(26);
@@ -36,26 +64,52 @@ double indexAlpha(string text, size_t period)
 int main()
 {
     freopen("input.in", "r", stdin);
-    freopen("output.out", "w", stdout);
+    generateVigenereSquare();
+    //printVigenereSquare();
+    //cout << "Enter a key: "; we use file input so it is TODO to make interface more nice (to be possible to enter the key in console)
+    string key;
+    cin >> key;
+
     string text = "";
     string token;
     while (cin >> token)
     {
         text += token;
     }
-    size_t period = 0, maxPeriod = 0;
-    double max = 0;
+    //delete all symbols except 'a'..'z' and 'A'..'Z' from the input text and then make all chars big xd (u know, like 'a' -> 'A" and 'b' -> 'B'...)
+    for (size_t i = 0; i < text.size(); ++i)
+    {
+        if (!((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z')))
+        {
+            text.erase(i, 1);
+        }
+        if (text[i] >= 'a' && text[i] <= 'z')
+        {
+            text[i] = text[i] - 'a' + 'A';
+        }
+    }
+    cout << text << std::endl;
+    //now encrypt the text
+    string cipherText = "";
+    for (size_t i = 0; i < text.size(); ++i)
+    {
+        //                                                                                                                                                      yourtextishereqwert
+        cipherText += vigenereSquare[text[i] - 'A'][key[i % key.size()]- 'A']; //key[i % key.size()] means.. u know, when u write your key under your text like keykeykeykeykeykeyke , it returns the index of the key's char, that comes under the your current text char
+    }
+    cout << cipherText << std::endl;
+    size_t period = 0;
+    double maxValue = 0, maxPeriod;
 
-    while (period <= 17)
+    while (period <= MAX_PERIOD)
     {
         auto value = indexAlpha(text, ++period);
-        cout << value << std::endl;
-        if (value > max)
+        //cout << value << ' ' << period << std::endl; //here we should try different ways to look for the period (the closest to 0.065 ...)
+        if (value > maxValue)
         {
-            max = value;
+            maxValue = value;
             maxPeriod = period;
         }
     }
-    cout << max;
+    cout << "We have found the period: " << maxPeriod << std::endl;
     return 0;
 }
