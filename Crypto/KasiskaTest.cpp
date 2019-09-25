324da48d3bad1c8c78ca08665369444078e38338
u-menya-lapki <komaroff404@gmail.com>
2019-09-26 01:04:10 +0300

    input improved, test added, minor improvements. vol 2

--- Crypto/KasiskaTest.cpp
+++ Crypto/KasiskaTest.cpp
@@ -1,40 +1,61 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using std::string;
 using std::vector;
 using std::cout;
 using std::cin;
 
-double indexAlpha (string text, size_t period) {
-    vector <double> currentAlpha(26);
+double indexAlpha(string text, size_t period)
+{
+    vector<double> currentAlpha(26);
     size_t currentSize = 0;
-    for (size_t i = 0; i < text.size(); i += period) {
-        ++currentAlpha[text[i]-'a'];
-        ++currentSize;
+    for (size_t i = 0; i < text.size(); i += period)
+    {
+        if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
+        {
+            size_t shift = 'a';
+            if (text[i] >= 'A' && text[i] <= 'Z') { shift = 'A'; }
+            ++currentAlpha[text[i] - shift];
+            ++currentSize;
+        }
     }
 
     double ans = 0;
 
-    for (auto i : currentAlpha) {
-        ans += pow(i/(double)currentSize,2);
+    for (auto i : currentAlpha)
+    {
+        ans += pow(i / (double) currentSize, 2);
     }
 
     return ans;
 }
 
-int main() {
-    string text;
-    cin >> text;
+int main()
+{
+    freopen("input.in", "r", stdin);
+    freopen("output.out", "w", stdout);
+    string text = "";
+    string token;
+    while (cin >> token)
+    {
+        text += token;
+    }
     size_t period = 0, maxPeriod = 0;
     double max = 0;
 
-    while (period != text.size()) {
-        value = indexAlpha(text, ++period)
-        if (max < value) {
+    while (period <= 17)
+    {
+        auto value = indexAlpha(text, ++period);
+        cout << value << std::endl;
+        if (value > max)
+        {
             max = value;
             maxPeriod = period;
         }
     }
+    cout << max;
+    return 0;
 }
