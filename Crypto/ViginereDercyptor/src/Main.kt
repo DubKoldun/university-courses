cc573dfd988c59af96b78edc2b234855aeb030e9
DubKoldun <epifantsev.vitaliy@gmail.com>
2020-09-17 23:19:49 +0300

    🎉 Start Videnere decryptor

---
+++ Crypto/ViginereDercyptor/src/Main.kt
@@ -0,0 +1,9 @@
+import java.io.File
+
+fun main() {
+    val cipherTextFile : File = File("/home/vutaliy/prod/ct_itmo/Crypto/ViginereDercyptor/tests/012.cipher")
+    val cipherText : String = cipherTextFile.readText()
+
+    
+    
+}
