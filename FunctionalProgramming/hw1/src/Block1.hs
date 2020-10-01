149b58d96e1ec2bf3a4b8131f50364cc0c056572
DubKoldun <epifantsev.vitaliy@gmail.com>
2020-10-01 21:03:51 +0300

    🚧 Functional Programming. Added some comments and done Task 2

--- FunctionalProgramming/hw1/src/Block1.hs
+++ FunctionalProgramming/hw1/src/Block1.hs
@@ -5,7 +5,9 @@ module Block1 where
 import Data.List ( elemIndex )
 import Data.Maybe ( fromJust )
 
--- Task 1. Weekday implementation 
+-- | Task 1. Weekday implementation 
+
+-- | Declare class for days of the week
 data Weekday 
   = Monday
   | Tuesday
@@ -17,7 +19,15 @@ data Weekday
   deriving Show
 
 allDays :: [Weekday]
-allDays = [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday]
+allDays = 
+  [ Monday
+  , Tuesday
+  , Wednesday
+  , Thursday
+  , Friday
+  , Saturday
+  , Sunday
+  ]
 
 instance Eq Weekday where 
   (==) :: Weekday -> Weekday -> Bool
@@ -30,26 +40,33 @@ instance Enum Weekday where
   fromEnum :: Weekday -> Int
   fromEnum day = fromJust $ elemIndex day allDays
 
+-- | Returns next day
 nextDay :: Weekday -> Weekday
 nextDay = succ
 
+-- | Returns day after `n` days
 afterDays :: Weekday -> Int -> Weekday 
 afterDays day n = toEnum $ fromEnum day + n
 
+-- | Сhecks if the current day is a weekend
 isWeekend :: Weekday -> Bool
 isWeekend Saturday = True
 isWeekned Sunday   = True
 isWeekned _        = False
 
+-- | Returns amount of days til the party
 daysToParty :: Weekday -> Int
 daysToParty day = (fromEnum Friday - fromEnum day) `mod` 7
 
 -- Task 2. Nat implementation
+
+-- | Declare class Nat
 data Nat 
   = Z 
   | S Nat
   deriving Show
 
+-- | Enum instance for Nat
 instance Enum Nat where 
   toEnum :: Int -> Nat
   toEnum x | x < 0     = error "negative isn't natural"
@@ -63,10 +80,12 @@ instance Enum Nat where
   fromEnum Z = 0
   fromEnum (S rest) = fromEnum rest + 1 
 
+-- | Eq instance for comparsion for equality
 instance Eq Nat where
   (==) :: Nat -> Nat -> Bool
   (==) x y = show x == show y
 
+-- | Ord instance for comparsion
 instance Ord Nat where
   (<=) :: Nat -> Nat -> Bool
   (<=) = toAbstractOperation (<=)
@@ -77,14 +96,43 @@ toAbstractOperation f x y = fromEnum x `f` fromEnum y
 toNatOperation :: (Int -> Int -> Int) -> Nat -> Nat -> Nat
 toNatOperation f x y = toEnum $ toAbstractOperation f x y
 
+-- | Returns sum of 2 Nat
 sum :: Nat -> Nat -> Nat
 sum = toNatOperation (+) 
 
+-- | Returns multiplication of 2 Nat
 mul :: Nat -> Nat -> Nat
 mul = toNatOperation (*)
 
+-- | Returns substraction of 2 Nat
+sub :: Nat -> Nat -> Nat
+sub Z     _     = Z
+sub x     Z     = x
+sub (S x) (S y) = sub x y 
 
+-- | Convert Int to Nat: using Enum function
+toNat :: Int -> Nat
+toNat = toEnum
 
+-- | Convert Nat to Int: using Enum function
+fromNat :: Nat -> Int
+fromNat = fromEnum
+
+-- | Parity check 
+isEven :: Nat -> Bool
+isEven Z         = True
+isEven (S Z)     = False
+isEven (S (S x)) = isEven x
+
+-- | Returns integer division
+natDiv :: Nat -> Nat -> Nat
+natDiv = toNatOperation div
+
+-- | Returns modulo
+natMod :: Nat -> Nat -> Nat
+natMod = toNatOperation mod
+
+-- Task 3. Tree implementation
 
 
 
