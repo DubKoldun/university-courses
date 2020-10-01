d2cdf8ef063e071840471161aa04bb6b3e13f792
DubKoldun <epifantsev.vitaliy@gmail.com>
2020-10-02 01:12:51 +0300

    🚀 Task 6 done. With comments

---
+++ FunctionalProgramming/hw1/src/Task6.hs
@@ -0,0 +1,19 @@
+module Task6 where -- TODO()
+
+import Data.Either (partitionEithers)
+
+-- | Takes list of 'Maybe' with lists and concat that
+maybeConcat 
+  :: [Maybe [a]] -- ^ given list of 'Maybe'
+  -> [a] -- ^ lists contains elements from internal 'Maybe' lists
+maybeConcat xs = concat $ concat <$> xs 
+
+-- | Takes list of 'Either a b', where types 'a' and 'b'
+-- implements 'Monoid' interface and return 'Pair' of 'Monoid'
+eitherConcat 
+  :: (Monoid a, Monoid b) -- ^ restriction on inner 'Either' types
+  => [Either a b] -- ^ given list of 'Either'
+  -> (a, b) -- ^ pair of concatinationg 'Monoid' 
+eitherConcat xs = (mconcat lefts, mconcat rights)
+  where
+    (lefts, rights) = partitionEithers xs  
