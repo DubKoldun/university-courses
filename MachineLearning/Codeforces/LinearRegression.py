7faed9e484e63ce1b747ddf50e5dfba76f54fb58
DubKoldun <epifantsev.vitaliy@gmail.com>
2021-01-10 16:08:50 +0300

    🎨 ML cf LR rewrite on numpy

--- MachineLearning/Codeforces/LinearRegression.py
+++ MachineLearning/Codeforces/LinearRegression.py
@@ -1,5 +1,22 @@
 import random as rnd
 from math import sqrt
+import numpy as np
+
+
+def min_max_x(objs, mins_and_maxs): 
+    for i in range(len(objs)):
+        for j in range(len(objs[0])):
+            objs[i][j] = (objs[i][j] - mins_and_maxs[j][0]) / (mins_and_maxs[j][1] - mins_and_maxs[j][0]) if mins_and_maxs[j][1] - mins_and_maxs[j][0] != 0 else 0
+
+def min_max_y(ys, min_and_max):
+    for i in range(len(ys)):
+        ys[i] = (ys[i] - min_and_max[0]) / (min_and_max[1] - min_and_max[0]) if min_and_max[1] - min_and_max[0] != 0 else 0
+
+
+def add_ones(objs):
+    for i in range(len(objs)):
+        objs[i].append(1)
+
 
 
 # file = open("/home/vutaliy/Downloads/LR/0.40_0.65.txt", "r") 
@@ -11,6 +28,7 @@ objects = []
 main_attribute = []
 
 min_max_obj = [[float("inf"), float("-inf")] for i in range(m) ]
+min_max_att = [float("inf"), float("-inf")]
 
 for i in range(n):
     
@@ -18,7 +36,14 @@ for i in range(n):
     inpt = input().split()
     buff = []
     
-    main_attribute.append(float(inpt[m]))
+    y = float(inpt[m])
+    main_attribute.append(y)
+
+    if (y < min_max_att[0]):
+        min_max_att[0] = y
+
+    if (y > min_max_att[1]):
+        min_max_att[1] = y
 
     for j in range(len(inpt) - 1):
         x = float(inpt[j])
@@ -31,7 +56,8 @@ for i in range(n):
             min_max_obj[j][1] = x
     
     objects.append(buff)
-blabla = 1
+
+
 # if (n == 2):
 #     print(31.0)
 #     print(-60420.0)
@@ -39,101 +65,36 @@ blabla = 1
 #     print(2.0)
 #     print(-1.0)
 # else:
-if blabla == 1:
+if 1 == 1:
     old_objects = [i.copy() for i in objects]
-
-    def min_max(objs, mins_and_maxs): 
-        for i in range(len(objs)):
-            for j in range(len(objs[0])):
-                objs[i][j] = (objs[i][j] - mins_and_maxs[j][0]) / (mins_and_maxs[j][1] - mins_and_maxs[j][0]) if mins_and_maxs[j][1] - mins_and_maxs[j][0] != 0 else 0
-
-    min_max(objects, min_max_obj)
-
-    def add_ones(objs):
-        for i in range(len(objs)):
-            objs[i].append(1)
-
+    min_max_x(objects, min_max_obj)
+    min_max_y(main_attribute, min_max_att)
     add_ones(objects)
     add_ones(old_objects)
 
-    def sign(vec):
-        buff = []
-        for obj in vec:
-            if (obj < 0):
-                buff.append(-1) 
-            elif (obj > 0):
-                buff.append(1)
-            else:
-                buff.append(0)
-        return buff
+    objects = np.array(objects)
 
-    def multiply_vec(vec1, vec2):
-        buff = []
-        for i in range(len(vec1)):
-            buff.append(vec1[i] * vec2[i])
-        return sum(buff)
-
-    def multiply_s_vec(x, vec):
-        buff = []
-        for i in vec:
-            buff.append(x * i)
-        return buff
+    def dif_smape(x, w, y): # need to calculate norm dif
+        buff = np.dot(w, x)
+        return (np.sign(buff - y) * x * (buff + y) - abs(buff - y) * x) / (buff + y) ** 2
 
     def dif_sq(x, w, y):
-        # print(x, w)
-        buff = multiply_vec(x, w)
-        # print(buff)
-        return multiply_s_vec(multiply_vec(x, w) - y, x)
-
-    def suma(lst):
-        buff = [0] * len(lst[0])
-        for obj in lst:
-            for i in range(len(obj)):
-                buff[i] += obj[i]
-        return buff
+        return np.dot((np.dot(x, w) - y), x)
 
     def empirical_risk(w, func, start, k):
-        for i in range(start, start + k + 1):
-            buff = func(objects[i], w, main_attribute[i])
+        return 2 / k * sum([func(objects[i], w, main_attribute[i]) for i in range(start, start + k + 1)])
 
-        lst = [func(objects[i], w, main_attribute[i]) for i in range(start, start + k + 1)]
-        # print(lst)
-        return multiply_s_vec(1 / k, suma(lst))
+    # amount = 5000, h = 10000, lmbd2 = 0.1
 
-    def diff(vec1, vec2):
-        buff = []
-        for i in range(len(vec1)):
-            buff.append(vec1[i] - vec2[i])
-        return buff
-
-    def add(vec1, vec2):
-        buff = []
-        for i in range(len(vec1)):
-            buff.append(vec1[i] + vec2[i])
-        return buff
-
-    ## algo
-
-    amount = 2000
-
-    def bgd(amount, batch_size = sqrt(n), h = 0.05, lmbd2 = 0.1):
-        lmbd = 0.01
-        start = rnd.randint(0, len(objects) - batch_size - 1)
-
-        # weights = [rnd.uniform(-1/(2 * m+1), 1/(2 * m+1)) for i in range(m+1)]
-        weights = [0 for i in range(m+1)]
+    amount = 5000
+    def bgd(amount, batch_size = 25, h = 0.0001, lmbd2 = 100000):
         
-        assessment = add(empirical_risk(weights, dif_sq, start, batch_size), multiply_s_vec(lmbd2, sign(weights)))
-
+        start = rnd.randint(0, len(objects) - batch_size - 1)
+        weights = np.array([0.0 for i in range(objects.shape[1])]) #rnd.uniform(-1/(2 * objects.shape[1]), 1/(2 * objects.shape[1]))
 
         for i in range(1, amount): 
             start = rnd.randint(0, len(objects) - batch_size - 1)
-            
-            buff = multiply_s_vec(h, assessment)
-            weights = diff(weights, buff)
-
-            keks =  add(empirical_risk(weights, dif_sq, start, batch_size), multiply_s_vec(lmbd2, sign(weights)))
-            assessment = add(multiply_s_vec((1 - lmbd), assessment), multiply_s_vec(lmbd, keks))
+            weights -= h * empirical_risk(weights, dif_smape, start, batch_size) + lmbd2 * np.sign(weights)
 
         return weights
 
@@ -141,35 +102,38 @@ if blabla == 1:
         sumAns = 0
 
         for i in range(len(ans)-1):
-            ans[i] /= (min_max_obj[i][1] - min_max_obj[i][0]) if min_max_obj[i][1] - min_max_obj[i][0] != 0 else 1
-            sumAns = sumAns + ans[i] * min_max_obj[i][0]
+            ans[i] /= (min_max_obj[i][1] - min_max_obj[i][0]) * (min_max_att[1] - min_max_att[0]) if min_max_obj[i][1] - min_max_obj[i][0] != 0 and min_max_att[1] - min_max_att[0] != 0  else 1
+            sumAns += ans[i] * min_max_obj[i][0] * min_max_att[0]
 
-        ans[len(ans)-1] -= sumAns 
+            ans[len(ans)-1] -= sumAns 
 
     def nmrse(preds_y, ans_y):
         sum_ = 0
-        min_p = min(preds_y)
-        max_p = max(preds_y)
+        min_p = min(ans_y)
+        max_p = max(ans_y)
         for ix, pred in enumerate(preds_y):
             sum_ += (pred - ans_y[ix]) ** 2
         return ((sum_ / len(preds_y)) ** 0.5) / (max_p - min_p) 
 
     # batch_size = min(2,int(sqrt(m)))
-    # ans = bgd(amount, batch_size)
-    # denormalize(ans)
-    # pred = [multiply_vec(ans, old_objects[i]) for i in range(len(objects))]
-    # print(nmrse(pred, main_attribute))
+    # best_reg = bgd(amount, batch_size)
+    # denormalize(best_reg)
+    # pred = [np.dot(best_reg, old_objects[i]) for i in range(len(objects))]
+    # print(ans)
 
     def find_best_reg():
         regs = []
         scores = []
-        for lmbd in range(0,3):
-            ans = bgd(amount, batch_size = min(2,int(sqrt(m))), lmbd2 = 1)
+
+        for lmbd in range(0,6):
+            ans = bgd(amount, batch_size = min(2,int(sqrt(m))))
             denormalize(ans)
+
             regs.append(ans)
-            y_pred = [multiply_vec(ans, old_objects[i]) for i in range(len(objects))]
+            y_pred = [np.dot(ans, old_objects[i]) for i in range(len(objects))]
             scores.append(nmrse(y_pred, main_attribute))
-        # print(scores)
+
+        print(scores)
         return regs[scores.index(min(scores))]
 
     best_reg = find_best_reg()
@@ -177,6 +141,8 @@ if blabla == 1:
     for i in best_reg:
         print(i)
 
-    # print(best_reg)
-    # best_reg2 = find_best_reg()
-    # print(best_reg2)
+    # print(nmrse(pred, main_attribute))
+
+    #     # print(best_reg)
+    #     # best_reg2 = find_best_reg()
+    #     # print(best_reg2)
